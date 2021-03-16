b6b52efc2be41f125df78df385031144cfe7fbeb
Abourass <abourassa@assetval.com>
2021-03-16 12:55:29 -0600

    :sparkles: New border color plguin created

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
 const production = !process.env.ROLLUP_WATCH;
 
 module.exports = {
@@ -5,7 +6,27 @@ module.exports = {
     removeDeprecatedGapUtilities: true,
     purgeLayersByDefault: true,
   },
-  plugins: [],
+  plugins: [
+    ({ addUtilities, e, theme, variants }) => {
+      let colors = flattenColorPalette(theme('borderColor'));
+      delete colors['default'];
+
+      // Replace or Add custom colors
+      if(this.theme?.extend?.colors !== undefined){
+        colors = Object.assign(colors, this.theme.extend.colors);
+      }
+
+      const colorMap = Object.keys(colors).map(color => ({
+        [`.border-t-${color}`]: {borderTopColor: colors[color]},
+        [`.border-r-${color}`]: {borderRightColor: colors[color]},
+        [`.border-b-${color}`]: {borderBottomColor: colors[color]},
+        [`.border-l-${color}`]: {borderLeftColor: colors[color]},
+      }));
+      const utilities = Object.assign({}, ...colorMap);
+
+      addUtilities(utilities, variants('borderColor'));
+    },
+  ],
   purge: {
     content: ["./src/**/*.svelte"],
     enabled: production,
@@ -13,9 +34,14 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        black: {
+          plum: '#323248',
+        },
         blue: {
+          primary: '#105f9e',
           450: '#105f9e',
           550: '#1b5f9e',
+          650: '#2b5a92'
         }
       }
     }
