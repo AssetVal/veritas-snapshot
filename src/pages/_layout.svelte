26811fb93516dc03e2843ad02c925dcbdc2d019c
Abourass <abourassa@assetval.com>
2021-03-16 08:23:42 -0600

    :sparkles: Beginning of the SPA log in created

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,10 +1,17 @@
 <script>
   import LogInHeader from '../../components/layout/LogInHeader.svelte';
+  import LogInForm from '../../components/layout/LogInForm.svelte';
+  import {user} from '../../stores/user';
 </script>
 
 <main class="flex flex-col items-center min-h-full">
-  <LogInHeader/>
-  <!-- routify:options preload="proximity" -->
-  <slot />
+
+  {#if $user}
+    <!-- routify:options preload="proximity" -->
+    <slot />
+  {:else}
+    <LogInHeader/>
+    <LogInForm />
+  {/if}
 </main>
 
