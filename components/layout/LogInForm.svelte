bb069b84961e2e8d2aae039042e195226a0c19b5
Abourass <abourassa@assetval.com>
2021-03-16 12:55:45 -0600

    :art: Created log in form

--- components/layout/LogInForm.svelte
+++ components/layout/LogInForm.svelte
@@ -1,25 +1,63 @@
 <script>
   import { user } from '../../stores/user';
-  import { goto, url } from '@roxi/routify';
+  import { goto } from '@roxi/routify';
 
-  let username, password;
+  let email, password;
 
   async function login(){
-    const response = await fetch('')
+    const response = await fetch('https://www.assetval.club/snapshotLogIn', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8'
+      },
+      body: JSON.stringify({email, password})
+    });
+
+    const result = await response.json();
+
+    console.log(result);
+
+    $user = { email, password };
+    console.log($user);
+
+    /** We want to $goto our current location.
+     *  Since we're now logged in, we shouldn't be redirected to this login page again. **/
+    $goto(window.location.href);
   }
 </script>
 
 <style>
-  .mw-420 {
-    min-width: 420px;
-  }
+  .mw-420 { min-width: 420px; }
 </style>
 
-<form class="pt-12 px-8 pb-4 rounded bg-white shadow w-100 relative mw-420">
-  <div class="m-4">
-    <div class="relative">
-      <input id="email" type="text" class="pt-5 px-3 pb-1 text-sm focus:none" autocomplete="email" required autofocus>
-      <label for="email">Email</label>
-    </div>
+<form class="pt-12 px-8 pb-4 rounded bg-white shadow relative mw-420">
+  <div class="m-4 relative">
+    <input
+      required
+      id="email"
+      type="text"
+      class="pt-5 px-3 pb-1 text-sm border-b-2 focus:outline-none focus:border-b-blue-primary focus:black-plum"
+      autocomplete="email"
+      bind:value={email}>
+    <label for="email">Email</label>
   </div>
+
+  <div class="m-4 relative">
+    <input
+      required
+      id="password"
+      type="password"
+      class="pt-5 px-3 pb-1 text-sm border-b-2 focus:outline-none focus:border-b-blue-primary focus:black-plum"
+      autocomplete="password"
+      bind:value={password}>
+    <label for="password">Password</label>
+  </div>
+
+  <div class="m-4 flex justify-center">
+    <button
+      class="h-11 py-2 px-4 rounded border-blue-650 bg-blue-primary text-white w-full"
+      on:click={login}
+    > Sign In </button>
+  </div>
+
 </form>
--- src/pages/example/_components/RoutifyIntro.svelte
+++ src/pages/example/_components/RoutifyIntro.svelte
@@ -1,6 +1,20 @@
-<h1>Routify Starter</h1>
+<script>
+  import { user } from '../../../../stores/user';
+  import {goto} from '@roxi/routify';
 
-To see an example app, go to 
-<a href="/example">/example</a>
+  async function logout(){
+    $user = false;
 
-<p>To delete the example app, simply delete the ./src/pages/example folder.</p>
+    /** We want to $goto our current location.
+     *  Since we're now logged in, we shouldn't be redirected to this login page again.
+     * **/
+    $goto(window.location.href);
+  }
+</script>
+
+<button
+  class="h-11 py-2 px-4 rounded border-blue-650 bg-blue-primary text-white mt-4 focus:outline-none"
+  on:click={logout}
+>
+  Log Out
+</button>
