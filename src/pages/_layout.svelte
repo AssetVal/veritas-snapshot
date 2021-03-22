73dd84280cc8e6bb5b41054b5d23393e4323daeb
Abourass <abourassa@assetval.com>
2021-03-22 13:55:06 -0600

    :art: Started work on the app header

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,13 +1,17 @@
 <script>
-  import { vendor } from '../../stores/vendor'
-  import { goto } from '@roxi/routify';
+  import {vendor} from '../../stores/vendor';
+  import {goto} from '@roxi/routify';
+  import Header from '../../components/layout/Header.svelte';
 
   $: if (!$vendor) $goto("./login", {}, true);
 </script>
 
 {#if $vendor}
+  <Header />
   <main class="flex flex-col items-center min-h-full">
-    <!-- routify:options preload="proximity" -->
-    <slot/>
+    <div class="w-full px-4 py-6">
+      <!-- routify:options preload="proximity" -->
+      <slot/>
+    </div>
   </main>
 {/if}
