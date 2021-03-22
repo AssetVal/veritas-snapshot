b33b4bd7e1c4fa537c8b90da7e31909cd544e081
Abourass <abourassa@assetval.com>
2021-03-22 06:49:41 -0600

    :art: Add the ability to be logged out once logged in

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,20 @@
 <script>
-    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
-    import { metatags } from '@roxi/routify'
-    metatags.title = 'Veritas Snapshot'
-    metatags.description = 'Veritas Snapshot - Take photos with ease'
+  import {vendor} from '../../stores/vendor';
+  import {goto} from '@roxi/routify';
+
+  async function logout(){
+    $vendor = false;
+
+    /** We want to $goto our current location.
+     *  Since we're now logged in, we shouldn't be redirected to this login page again.
+     * **/
+    $goto(window.location.href);
+  }
 </script>
 
-<RoutifyIntro />
+<h1>Welcome Home</h1>
+<button
+  class="h-11 py-2 px-4 rounded border-blue-650 bg-blue-primary text-white mt-4 focus:outline-none"
+  on:click={logout}>
+  Log Out {$vendor.user.name.first}
+</button>
