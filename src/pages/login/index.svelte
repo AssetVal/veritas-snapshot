e7bae1271cda7ab2f6b944767874beb66795f8a1
Abourass <abourassa@assetval.com>
2021-03-22 06:50:02 -0600

    :sparkles: Move login to a full page so it can be prerendered

---
+++ src/pages/login/index.svelte
@@ -0,0 +1,9 @@
+<script>
+  import LogInHeader from '../../../components/layout/LogInHeader.svelte';
+  import LogInForm from '../../../components/layout/LogInForm.svelte';
+</script>
+
+<main class="flex flex-col items-center min-h-full">
+  <LogInHeader />
+  <LogInForm />
+</main>
