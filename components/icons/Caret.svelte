c84df89e66d2a2463edc91692f26a56c598970a6
Abourass <abourassa@assetval.com>
2021-03-23 12:23:50 -0600

    :sparkles: Created a Caret component

---
+++ components/icons/Caret.svelte
@@ -0,0 +1,33 @@
+<style>
+  .account-icon {
+    position: absolute;
+    right: 1rem;
+    top: 50%;
+    transform: translateY(-50%);
+    opacity: .7;
+    transition: transform 200ms ease-in-out;
+  }
+  .caret-icon {
+    font-size: 1.3333333333em;
+    line-height: .75em;
+    vertical-align: -.225em;
+    width: .625em;
+    overflow: visible;
+  }
+</style>
+
+<span class="account-icon">
+  <svg
+    class="caret-icon"
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fa"
+    data-icon="caret-down"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 320 512"
+    data-fa-i2svg=""
+  >
+    <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+  </svg>
+</span>
