52fe0680ab98e8e864cd99e8dae7c98d658283d8
Abourass <abourassa@assetval.com>
2021-03-23 08:12:44 -0600

    :sparkles: Created a logo component

---
+++ components/layout/Logo.svelte
@@ -0,0 +1,13 @@
+<style>
+  .logo-img {
+    margin-bottom: 0.3rem; padding-right:0.7rem;
+  }
+</style>
+
+<span style="line-height: 0.85!important;">
+  <img
+    alt="AssetVal"
+    src="https://s3.us-west-1.amazonaws.com/assetvalphotos/assets/Asset_val_v2.png"
+    class="logo-img"
+  >
+</span>
