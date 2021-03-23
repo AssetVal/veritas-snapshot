a1f1b44d8924660e8b4f7bf9e69e834377f0ba6d
Abourass <abourassa@assetval.com>
2021-03-23 08:11:20 -0600

    :art: Sidebar added

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -2,13 +2,17 @@
   import {vendor} from '../../stores/vendor';
   import {goto} from '@roxi/routify';
   import Header from '../../components/layout/Header.svelte';
+  import Sidebar from '../../components/layout/Sidebar.svelte';
+
+  let sidebarOpen = false;
 
   $: if (!$vendor) $goto("./login", {}, true);
 </script>
 
 {#if $vendor}
-  <Header />
-  <main class="flex flex-col items-center min-h-full">
+  <Header bind:sidebar={sidebarOpen}/>
+  <Sidebar bind:open={sidebarOpen}/>
+  <main class="flex flex-col items-center min-h-full" on:click={() => sidebarOpen = false}>
     <div class="w-full px-4 py-6">
       <!-- routify:options preload="proximity" -->
       <slot/>
