b898864bc2638dd7d714b5019baf50b413938c16
Abourass <abourassa@assetval.com>
2021-03-23 14:29:08 -0600

    :art: Sidebar menu functional

--- components/layout/SidebarHeader.svelte
+++ components/layout/SidebarHeader.svelte
@@ -4,15 +4,17 @@
   import {goto} from '@roxi/routify';
   // Props
   export let sidebarOpen = false;
+
   // Variables
   let classes;
   let expandClass;
   let subDropdownOpen = false;
+
   // Reactive
   $: (sidebarOpen) ? (classes = 'close-btn show') : (classes = 'close-btn')
   $: (subDropdownOpen) ? (expandClass = 'dropdown-aside show bg-white') : (expandClass = 'dropdown-aside collapse')
 
-  // Work
+  // Debug
   console.log($vendor);
 
   // Functions
@@ -23,6 +25,22 @@
 </script>
 
 <style>
+  .account-icon {
+    position: absolute;
+    right: 1rem;
+    top: 50%;
+    transform: translateY(-50%);
+    opacity: .7;
+    transition: transform 200ms ease-in-out;
+  }
+  .caret-icon {
+    font-size: 1.3333333333em;
+    line-height: .75em;
+    vertical-align: -.225em;
+    width: .625em;
+    overflow: visible;
+  }
+
   .sidebar-header {
     position: relative;
     display: block;
@@ -76,12 +94,26 @@
     z-index: 2;
   }
 
-  .btn-account[aria-expanded=true]>.account-icon>.caret-icon {
+  .btn-account[aria-expanded=true]>.account-icon {
     transform: rotate(180deg);
+    top: 39%
   }
 
   .collapse:not(.show) {
     display: none;
+  }
+
+  .description {
+    font-size: .875em;
+    opacity: .7;
+  }
+
+  .d-item { color: #15152d; }
+
+  .d-item:hover {
+    color: #fff;
+    text-decoration: none;
+    background-color: #105f9e;
   }
 </style>
 
@@ -90,7 +122,7 @@
     <span class="text-white text-3xl -mx-4">X</span>
   </button>
   <button
-    class="p-4 w-full flex flex-row border-none items-center btn-account md:hidden lg:hidden xl:hidden"
+    class="p-4 w-full flex flex-row border-none items-center btn-account md:hidden lg:hidden xl:hidden focus:outline-none"
     type="button"
     aria-expanded={subDropdownOpen}
     on:click={() => (subDropdownOpen) ? (subDropdownOpen = false) : (subDropdownOpen = true)}
@@ -100,12 +132,26 @@
         <img class="rounded-full" alt="User Avatar" src="https://s3.us-west-1.amazonaws.com/veritasprofile/{$vendor.user._id}-profile.webp">
       {/if}
     </span>
-    <Caret />
+    <span class="account-icon">
+      <svg
+        class="caret-icon"
+        aria-hidden="true"
+        focusable="false"
+        data-prefix="fa"
+        data-icon="caret-down"
+        role="img"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 320 512"
+        data-fa-i2svg=""
+      >
+        <path fill="currentColor" d="M31.3 192h257.3c17.8 0 26.7 21.5 14.1 34.1L174.1 354.8c-7.8 7.8-20.5 7.8-28.3 0L17.2 226.1C4.6 213.5 13.5 192 31.3 192z"></path>
+      </svg>
+    </span>
     <span class="mr-3 block text-left flex-1 overflow-hidden overflow-clip">
       <span class="text-lg block overflow-hidden overflow-ellipsis whitespace-nowrap font-semibold">
         {$vendor.user.name.nickname} {$vendor.user.name.last}
       </span>
-      <span class="text-xs block overflow-hidden overflow-ellipsis whitespace-nowrap">
+      <span class="block overflow-hidden overflow-ellipsis whitespace-nowrap description">
         {$vendor.user.desc}
       </span>
     </span>
@@ -113,7 +159,7 @@
   <div class={expandClass}>
     <div class="pb-3">
       <a href="#" class="block w-full px-4 py-2 d-item">Settings</a>
-      <button class="block w-full px-4 py-2 d-item" on:click={logout}>Logout</button>
+      <button class="block w-full px-4 py-2 d-item -mb-3 text-left focus:outline-none" on:click={logout}>Logout</button>
     </div>
   </div>
 </header>
