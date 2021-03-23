b6730a59be99f3241b6cf578379086deb58947b6
Abourass <abourassa@assetval.com>
2021-03-23 12:24:04 -0600

    :art: Removed the fake logout

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,16 +1,8 @@
 <script lang="ts">
   import {vendor} from '../../stores/vendor';
-  import {goto} from '@roxi/routify';
   import ChooseOrderCard from '../_components/chooseOrder/ChooseOrderCard.svelte';
   import Card from '../_components/layout/Card.svelte';
   import ChooseOrderHeader from '../_components/chooseOrder/ChooseOrderHeader.svelte';
-
-  async function logout() {
-    $vendor = null;
-    /** We want to $goto our current location.
-     *  Since we're now logged in, we shouldn't be redirected to this login page again.  **/
-    $goto(window.location.href);
-  }
 </script>
 
 <ChooseOrderHeader totalOrders={$vendor.orders.inProgress.length}/>
@@ -26,9 +18,3 @@
     </Card>
   {/if}
 </div>
-
-<button
-  class="h-11 py-2 px-4 rounded border-blue-650 bg-blue-primary text-white mt-4 focus:outline-none"
-  on:click={logout}>
-  Log Out {$vendor.user.name.first}
-</button>
