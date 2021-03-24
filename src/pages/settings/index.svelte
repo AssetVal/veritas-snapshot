0e18036eaa5ff1b46b91daca83323cf9ab823575
abourass <abourassa@assetval.com>
2021-03-24 10:39:10 -0600

    :sparkles: Create placeholder for settings page

---
+++ src/pages/settings/index.svelte
@@ -0,0 +1,16 @@
+
+<header class="mx-auto p-0 block">
+  <h1 class="mt-0 mr-auto text-3xl font-semibold mb-2">Settings</h1>
+</header>
+
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
+<h1>Settings</h1>
