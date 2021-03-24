c78bc22814128c6ab627479f671d36538762c710
abourass <abourassa@assetval.com>
2021-03-24 10:39:33 -0600

    :bug: Fixed issue where header would block part of the page

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -12,7 +12,7 @@
 {#if $vendor}
   <Header bind:sidebar={sidebarOpen}/>
   <Sidebar bind:open={sidebarOpen}/>
-  <main class="flex flex-col items-center min-h-full" on:click={() => sidebarOpen = false}>
+  <main class="flex flex-col items-center min-h-full mt-8" on:click={() => sidebarOpen = false}>
     <div class="w-full px-4 py-6">
       <!-- routify:options preload="proximity" -->
       <slot/>
