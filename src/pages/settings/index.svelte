1a00a3f782e2a6a3cb42363363eaddff60488987
abourass <abourassa@assetval.com>
2021-03-24 16:19:11 -0600

    :art: Added option to change your preferred map app

--- src/pages/settings/index.svelte
+++ src/pages/settings/index.svelte
@@ -1,8 +1,18 @@
-<script>
+<script lang="ts">
+  import { vendor } from '../../../stores/vendor';
   import SubHeading from '../../../components/layout/SubHeading.svelte';
+
+  const mapOptions: Array<'Google Maps'|'Waze'> = ['Google Maps', 'Waze']
 </script>
 
 <SubHeading> Settings </SubHeading>
+
+<label for="preferredMapApp">What is your preferred Map App</label>
+<select bind:value={$vendor.preferredMapApp} id="preferredMapApp">
+  {#each mapOptions as mapOption}
+    <option value={mapOption}>{mapOption}</option>
+  {/each}
+</select>
 
 <h1>Settings</h1>
 <h1>Settings</h1>
