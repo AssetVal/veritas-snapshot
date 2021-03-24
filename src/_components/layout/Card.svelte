c8c1f5a3dc3d24892b88514853a6364740b05c58
abourass <abourassa@assetval.com>
2021-03-24 13:36:58 -0600

    :art: Made the card's fullwidth

--- src/_components/layout/Card.svelte
+++ src/_components/layout/Card.svelte
@@ -1,4 +1,4 @@
-<div class="relative flex flex-col break-words rounded border-none mb-5 bg-white bg-clip-border shadow-md">
+<div class="relative flex flex-col break-words rounded border-none mb-5 bg-white bg-clip-border w-full shadow-md">
   <div class="flex-auto p-4">
     <slot />
   </div>
