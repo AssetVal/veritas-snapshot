c1a87b35ff050aefbe63ab04eee3af45b0ed2c37
abourass <abourassa@assetval.com>
2021-03-24 13:37:32 -0600

    :art: Moved to using the wrapper component

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import {vendor} from '../../stores/vendor';
   import ChooseOrderCard from '../_components/chooseOrder/ChooseOrderCard.svelte';
-  import Card from '../_components/layout/Card.svelte';
   import SubHeading from '../../components/layout/SubHeading.svelte';
+  import Wrapper from '../../components/layout/Wrapper.svelte';
+  import Card from '../_components/layout/Card.svelte';
 </script>
 
 <SubHeading>
@@ -10,7 +11,7 @@
   <small class="text-base text-coolGray-400 text-center">{$vendor.orders.inProgress.length} Total</small>
 </SubHeading>
 
-<div class="h-full relative flex flex-wrap -mx-2.5">
+<Wrapper>
   {#if $vendor.orders.inProgress.length > 0}
     {#each $vendor.orders.inProgress as order}
       <ChooseOrderCard order={order}/>
@@ -20,4 +21,4 @@
       <h3>No orders in progress</h3>
     </Card>
   {/if}
-</div>
+</Wrapper>
