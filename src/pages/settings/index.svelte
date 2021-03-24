e50933cf5f7b23fbc1ce29e46a5cb6050ea2a131
abourass <abourassa@assetval.com>
2021-03-24 17:25:21 -0600

    :sparkles: Settings page is done for now

--- src/pages/settings/index.svelte
+++ src/pages/settings/index.svelte
@@ -2,15 +2,28 @@
   import { vendor } from '../../../stores/vendor';
   import SubHeading from '../../../components/layout/SubHeading.svelte';
 
-  const mapOptions: Array<'Google Maps'|'Waze'> = ['Google Maps', 'Waze']
+  interface iPush { value: boolean, text: 'Yes'|'No' }
+
+  const mapOptions: Array<'Google Maps'|'Waze'> = ['Google Maps', 'Waze'];
+  const pushOptions:Array<iPush> = [{value: false, text: 'No'}, {value: true, text: 'Yes'}];
 </script>
 
 <SubHeading> Settings </SubHeading>
 
-<label for="preferredMapApp">What is your preferred Map App</label>
-<select bind:value={$vendor.preferredMapApp} id="preferredMapApp" class="select">
-  {#each mapOptions as mapOption}
-    <option value={mapOption}>{mapOption}</option>
-  {/each}
-</select>
+<div class="pb-2">
+  <label for="preferredMapApp">What is your preferred Map App</label>
+  <select bind:value={$vendor.preferredMapApp} id="preferredMapApp" class="select">
+    {#each mapOptions as mapOption}
+      <option value="{mapOption}">{mapOption}</option>
+    {/each}
+  </select>
+</div>
 
+<div class="pb-2">
+  <label for="allowPushNotifications">Allow Push Notifications</label>
+  <select bind:value={$vendor.allowPushNotifications} id="allowPushNotifications" class="select">
+    {#each pushOptions as option}
+      <option value={option.value}>{option.text}</option>
+    {/each}
+  </select>
+</div>
