b147fd99a0c03dae01413f4677c885f5fc96c486
abourass <abourassa@assetval.com>
2021-03-24 14:44:48 -0600

    :sparkles: Added a footer slot to the card component

--- src/_components/layout/Card.svelte
+++ src/_components/layout/Card.svelte
@@ -1,5 +1,9 @@
 <div class="relative flex flex-col break-words rounded border-none mb-5 bg-white bg-clip-border w-full shadow-md">
   <div class="flex-auto p-4">
-    <slot />
+    <slot name="body" />
   </div>
+
+  {#if  $$slots.footer}
+    <slot name="footer" />
+  {/if}
 </div>
