db4267a1e5e21d8576b052d9ffef1252eaff1972
abourass <abourassa@assetval.com>
2021-03-25 17:59:23 -0600

    :sparkles: Place holder order page made

---
+++ src/pages/order/index.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { params } from '@roxi/routify';
+  import {vendor} from '../../../stores/vendor';
+  import Order from '../../../classes/Order';
+
+  let order: Order = $vendor.orders.inProgress.filter((order: Order) => order._id === $params._id)[0];
+</script>
+
+
+<div>
+  <h1>{order.address.street}</h1>
+</div>
