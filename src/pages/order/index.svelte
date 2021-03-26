9dd7dc118d860317a93ecd89a1911011d2df2bf7
abourass <abourassa@assetval.com>
2021-03-26 15:20:37 -0600

    :sparkles: Take photos!

--- src/pages/order/index.svelte
+++ src/pages/order/index.svelte
@@ -1,12 +1,24 @@
 <script lang="ts">
+  import { Dashboard } from '@uppy/svelte'
+  import Uppy from '@uppy/core'
+  import Webcam from '@uppy/webcam'
   import { params } from '@roxi/routify';
   import {vendor} from '../../../stores/vendor';
   import Order from '../../../classes/Order';
 
   let order: Order = $vendor.orders.inProgress.filter((order: Order) => order._id === $params._id)[0];
+  let uppy = new Uppy().use(Webcam);
 </script>
 
+<style global>
+  @import '@uppy/core/dist/style.css';
+  @import '@uppy/dashboard/dist/style.css';
+</style>
 
 <div>
   <h1>{order.address.street}</h1>
 </div>
+<Dashboard
+  uppy={uppy}
+  plugins={["Webcam"]}
+/>
