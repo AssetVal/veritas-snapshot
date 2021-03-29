1c21b64bb99aedd2937efdbb3b83e528a3164eb1
Abourass <abourassa@assetval.com>
2021-03-29 14:24:19 -0600

    :sparkles: Info header created

--- src/pages/order/_reset.svelte
+++ src/pages/order/_reset.svelte
@@ -4,29 +4,46 @@
   import {vendor} from '../../../stores/vendor';
   import {goto} from '@roxi/routify';
   import {params} from '@roxi/routify';
-  import { order } from '../../../stores/order';
+  import {order} from '../../../stores/order';
   import Order from '../../../classes/Order';
+  import BackIcon from '../../../components/icons/BackIcon.svelte';
 
   $order = $vendor?.orders.inProgress.filter((order: Order) => order._id === $params._id)[0];
 
   let sidebarOpen = false;
 
-  $: if (!$vendor) $goto("../login",);
+  $: if (!$vendor) $goto('../login',);
 </script>
+
+<style>
+  .h-fit {height: fit-content}
+</style>
 
 {#if $vendor}
   <Header bind:sidebar={sidebarOpen}>
-    <div class="bg-dark-transparent flex flex-col px-3 -ml-2 h-full opacity-90" slot="infoArea">
-      {#if $order?.address.street}
+    <div class="flex flex-row justify-between w-full" slot="infoArea">
+      <div class="bg-dark-transparent flex flex-col px-3 -ml-2 h-full opacity-90">
+        {#if $order?.address.street}
         <span class="text-xl text-white opacity-100">
           {$order.address.street}{#if $order.address.unitNumber}, {$order.address.unitNumber}{/if}
         </span>
-      {/if}
-      {#if $order?.address.city}
-        <small class="text-lg text-white">
-          {$order?.address.city}, {$order?.address.state}, {$order?.address.zip}
-        </small>
-      {/if}
+        {/if}
+        {#if $order?.address.city}
+          <small class="text-lg text-white">
+            {$order?.address.city}, {$order?.address.state}, {$order?.address.zip}
+          </small>
+        {/if}
+      </div>
+      <div
+        class="bg-dark-transparent rounded-full h-fit my-auto mr-4"
+        on:click={() => $goto('/')}
+      >
+        <BackIcon
+          colorClass="text-white"
+          height="2rem"
+          width="2rem"
+        />
+      </div>
     </div>
   </Header>
   <Sidebar bind:open={sidebarOpen}/>
