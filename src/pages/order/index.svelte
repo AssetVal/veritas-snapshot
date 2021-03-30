c4557bfd302a20567c3a7e4a8b09d87bed236fc6
Abourass <abourassa@assetval.com>
2021-03-30 11:28:35 -0600

    :sparkles: Now handles both interior and exterior photos

--- src/pages/order/index.svelte
+++ src/pages/order/index.svelte
@@ -1,28 +1,13 @@
 <script lang="ts">
-  import { Dashboard } from '@uppy/svelte';
-  import { order } from '../../../stores/order';
-  import uppyInstance from './_modules/uppyFactory';
+  import {order} from '../../../stores/order';
   import changeUppyTheme from './_modules/changeUppyTheme';
+  import ExteriorPhotos from './_components/ExteriorPhotos.svelte';
+  import InteriorPhotos from './_components/InteriorPhotos.svelte';
 
-  console.log($order)
-
-  const exteriorPhotoCategories = [
-    {text: 'Front View', id: 'frontView', required: true},
-    {text: 'Front Angled View', id: 'frontAngledView', required: true},
-    {text: 'Front View Right', id: 'frontViewRight', required: true},
-    {text: 'Front View Left', id: 'frontViewLeft', required: true},
-    {text: 'Street View', id: 'streetView', required: true},
-    {text: 'Opposite Street View', id: 'oppositeStreetView', required: true},
-    {text: 'Right Side of Dwelling', id: 'rightSideOfDwelling', required: true},
-    {text: 'Left Side of Dwelling', id: 'leftSideOfDwelling', required: true},
-    {text: 'Rear of Dwelling', id: 'rearOfDwelling', required: true},
-    {text: 'Backyard', id: 'backyard', required: true},
-    {text: 'Address Verification', id: 'addressVerification', required: true},
-    {text: 'Exterior', id: 'exterior', required: false}
-  ];
+  console.log($order);
 
   setTimeout(changeUppyTheme, 180);
-  window.onresize = changeUppyTheme
+  window.onresize = changeUppyTheme;
 </script>
 
 <style global>
@@ -40,21 +25,7 @@
   [data-uppy-drag-drop-supported=true] .uppy-Dashboard-AddFiles { border: none !important; }
 </style>
 
-<div class="h-full grid grid-cols-2 gap-3 pt-4">
-  {#each exteriorPhotoCategories as category, index}
-    <div class="">
-      <div class="flex justify-center">
-        <small class="text-center">{category.text}</small>
-      </div>
-      <Dashboard
-        uppy={uppyInstance()}
-        props={{
-        inline: true,
-        height: 230,
-        proudlyDisplayPoweredByUppy: false,
-      }}
-      />
-    </div>
-  {/each}
-</div>
-
+<ExteriorPhotos />
+{#if $order.services.isInterior}
+  <InteriorPhotos />
+{/if}
