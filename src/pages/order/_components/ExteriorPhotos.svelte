81c8d25f8a6a395dd23f30eddc5b827992bd3536
Abourass <abourassa@assetval.com>
2021-03-30 11:28:06 -0600

    :sparkles: Created ExteriorPhotos.svelte

---
+++ src/pages/order/_components/ExteriorPhotos.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import {Dashboard} from '@uppy/svelte';
+  import uppyInstance from '../_modules/uppyFactory';
+  import SubHeading from '../../../../components/layout/SubHeading.svelte';
+
+  const exteriorPhotoCategories = [
+    {text: 'Front View', id: 'frontView', required: true},
+    {text: 'Front Angled View', id: 'frontAngledView', required: true},
+    {text: 'Front View Right', id: 'frontViewRight', required: true},
+    {text: 'Front View Left', id: 'frontViewLeft', required: true},
+    {text: 'Street View', id: 'streetView', required: true},
+    {text: 'Opposite Street View', id: 'oppositeStreetView', required: true},
+    {text: 'Right Side of Dwelling', id: 'rightSideOfDwelling', required: true},
+    {text: 'Left Side of Dwelling', id: 'leftSideOfDwelling', required: true},
+    {text: 'Rear of Dwelling', id: 'rearOfDwelling', required: true},
+    {text: 'Backyard', id: 'backyard', required: true},
+    {text: 'Address Verification', id: 'addressVerification', required: true},
+    {text: 'Exterior', id: 'exterior', required: false}
+  ];
+</script>
+
+<SubHeading>Exterior Photos</SubHeading>
+<div class="h-full grid grid-cols-2 gap-3">
+  {#each exteriorPhotoCategories as category, index}
+    <div>
+      <div class="flex justify-center">
+        <small class="text-center">{category.text}</small>
+      </div>
+      <Dashboard
+        uppy={uppyInstance()}
+        props={{
+          inline: true,
+          height: 230,
+          proudlyDisplayPoweredByUppy: false,
+        }}
+      />
+    </div>
+  {/each}
+</div>
