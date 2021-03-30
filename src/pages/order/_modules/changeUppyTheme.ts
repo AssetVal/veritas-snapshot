58ea2e2729663b019d9f13fe47e9ee7c1c94593f
Abourass <abourassa@assetval.com>
2021-03-30 11:27:54 -0600

    :bug: Fixed issue where if the phone is big enough to render the full text it lacked a space

--- src/pages/order/_modules/changeUppyTheme.ts
+++ src/pages/order/_modules/changeUppyTheme.ts
@@ -1,6 +1,6 @@
 export default function changeUppyTheme(){
   document.querySelectorAll('.uppy-Dashboard-AddFiles-title').forEach(el => {
-    el.childNodes[0].textContent = 'Click here';
+    el.childNodes[0].textContent = 'Click here ';
     el.childNodes[1].textContent = 'to browse or shoot';
     el.firstElementChild.classList.add('center-text')
   });
