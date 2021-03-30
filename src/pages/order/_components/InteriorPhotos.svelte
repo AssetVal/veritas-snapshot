c4bafbcb9435618d6475c9c28abff061594ab8db
Abourass <abourassa@assetval.com>
2021-03-30 11:28:20 -0600

    :sparkles: Created InteriorPhotos.svelte

---
+++ src/pages/order/_components/InteriorPhotos.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import Uppy from '@uppy/core';
+  import {Dashboard} from '@uppy/svelte';
+  import SubHeading from '../../../../components/layout/SubHeading.svelte';
+
+  const uppy = new Uppy({
+    autoProceed: true,
+    restrictions: {
+      maxFileSize: 10000000,
+      allowedFileTypes: ['image/*']
+    }});
+</script>
+
+<SubHeading>Interior Photos</SubHeading>
+<div class="h-full grid grid-cols-1 gap-3">
+  <Dashboard
+    uppy={uppy}
+    props={{
+      inline: true,
+      height: 300,
+      proudlyDisplayPoweredByUppy: false,
+    }}
+  />
+</div>
