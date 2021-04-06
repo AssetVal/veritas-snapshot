6dd7228644f8d758168bc4959ad82b1385eabb87
Abourass <abourassa@assetval.com>
2021-04-06 08:49:47 -0600

    :art: Now has a dummy URL it points to

--- src/pages/order/_modules/uppyFactory.ts
+++ src/pages/order/_modules/uppyFactory.ts
@@ -1,6 +1,7 @@
 import Uppy from '@uppy/core';
+import XHRUpload from '@uppy/xhr-upload';
 
-export default function uppyInstance(){
+export default function uppyInstance(suffix: string){
   // @ts-ignore
   return new Uppy({
     autoProceed: true,
@@ -8,5 +9,10 @@ export default function uppyInstance(){
       maxFileSize: 10000000,
       maxNumberOfFiles: 1,
       allowedFileTypes: ['image/*']
-    }});
+    }}).use(XHRUpload, {
+      endpoint: `http://192.168.245.108:5000/externalAPI/${suffix}`,
+      formData: true,
+      fieldName: 'file',
+      bundle: true,
+  });
 }
