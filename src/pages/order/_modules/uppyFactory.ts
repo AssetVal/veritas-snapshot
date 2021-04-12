80d401f9fff17ae455c285aa51d763bfd36ef27b
Abourass <abourassa@assetval.com>
2021-04-12 13:58:51 -0600

    :recycle: Cleaned up the code and made the uppy config more self documenting

--- src/pages/order/_modules/uppyFactory.ts
+++ src/pages/order/_modules/uppyFactory.ts
@@ -2,6 +2,8 @@ import Uppy from '@uppy/core';
 import XHRUpload from '@uppy/xhr-upload';
 
 export default function uppyInstance(maxPhotos: number, orderID: string, imageCategory: string){
+  const intExt = (maxPhotos > 1) ? 'interior' : 'exterior';
+
   // @ts-ignore
   return new Uppy({
     autoProceed: true,
@@ -10,9 +12,7 @@ export default function uppyInstance(maxPhotos: number, orderID: string, imageCa
       maxNumberOfFiles: maxPhotos,
       allowedFileTypes: ['image/*']
     }}).use(XHRUpload, {
-      endpoint: `https://www.assetval.club/api/snapshotUpload/${orderID}/${
-        (maxPhotos > 1) ? 'interior' : 'exterior'
-      }/${imageCategory}`,
+      endpoint: `https://www.assetval.club/api/snapshotUpload/${orderID}/${intExt}/${imageCategory}`,
       formData: true,
       fieldName: 'files',
       bundle: true,
