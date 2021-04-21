3c44b53b420498f91e3b9e8ea6f21d443011d014
abourass <abourassa@assetval.com>
2021-04-21 12:59:32 -0600

    :sparkles: Create toast with Veritas notification

--- src/pages/order/_modules/uppyFactory.ts
+++ src/pages/order/_modules/uppyFactory.ts
@@ -1,11 +1,12 @@
 import Uppy from '@uppy/core';
 import XHRUpload from '@uppy/xhr-upload';
+import { toast } from '@zerodevx/svelte-toast'
 
 export default function uppyInstance(maxPhotos: number, orderID: string, imageCategory: string){
   const intExt = (maxPhotos > 1) ? 'interior' : 'exterior';
 
   // @ts-ignore
-  return new Uppy({
+  const uppy =  new Uppy({
     autoProceed: true,
     restrictions: {
       maxFileSize: 10000000,
@@ -17,4 +18,19 @@ export default function uppyInstance(maxPhotos: number, orderID: string, imageCa
       fieldName: 'files',
       bundle: true,
   });
+
+  uppy.on('complete', async(result): Promise<void> => {
+    console.log('successful files:', result.successful);
+    console.log('failed files:', result.failed);
+
+    if (result.successful.length > 0){
+      if (result.successful.length === 1){ // Exterior Photo
+        const exteriorPhoto = result.successful[0];
+        const veritasResponse: { message: string } = exteriorPhoto.response.body;
+        toast.push(veritasResponse.message)
+      }
+    }
+  });
+
+  return uppy;
 }
