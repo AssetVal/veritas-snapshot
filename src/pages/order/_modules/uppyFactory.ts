b973eae6f8c5daab7ea9a9af971573617e10779e
abourass <abourassa@assetval.com>
2021-04-21 18:43:12 -0600

    :bulb: Added typescript definitions

--- src/pages/order/_modules/uppyFactory.ts
+++ src/pages/order/_modules/uppyFactory.ts
@@ -1,7 +1,9 @@
 import Uppy from '@uppy/core';
 import XHRUpload from '@uppy/xhr-upload';
-import { toast } from '@zerodevx/svelte-toast'
+import { toast } from '@zerodevx/svelte-toast';
 import toastThemes from '../../../_modules/toastThemes';
+import type Order from '../../../../classes/Order';
+import type {photoCategoryIDs} from '../_data/exteriorPhotoCategories';
 
 interface VeritasResponse {
   message: string,
@@ -9,7 +11,7 @@ interface VeritasResponse {
   href: string|Array<string>
 }
 
-export default function uppyInstance(maxPhotos: number, orderID: string, imageCategory: string){
+export default function uppyInstance(maxPhotos: number, order: Order, imageCategory: photoCategoryIDs){
   const intExt = (maxPhotos > 1) ? 'interior' : 'exterior';
 
   // @ts-ignore
@@ -20,7 +22,7 @@ export default function uppyInstance(maxPhotos: number, orderID: string, imageCa
       maxNumberOfFiles: maxPhotos,
       allowedFileTypes: ['image/*']
     }}).use(XHRUpload, {
-      endpoint: `https://www.assetval.club/api/snapshotUpload/${orderID}/${intExt}/${imageCategory}`,
+      endpoint: `https://www.assetval.club/api/snapshotUpload/${order._id}/${intExt}/${imageCategory}`,
       formData: true,
       fieldName: 'files',
       bundle: true,
@@ -36,7 +38,8 @@ export default function uppyInstance(maxPhotos: number, orderID: string, imageCa
         const {status, message, href}: VeritasResponse = exteriorPhoto.response.body;
 
         if (status === 'success'){
-          toast.push(message, toastThemes.success)
+          toast.push(message, toastThemes.success);
+          order.photos.exteriorFiles = [...order.photos.exteriorFiles, {name: href as string, category: imageCategory}]
         } else if (status === 'error') {
           toast.push(message, toastThemes.error)
         } else {
