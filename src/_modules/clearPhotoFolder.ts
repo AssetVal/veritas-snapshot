426c41735fabf53c4e724bd3b73eb71f5aa8e969
abourass <abourassa@assetval.com>
2021-04-22 22:38:57 -0600

    :sparkles: clearPhotoFolder created to clear entire exterior or interior folder at once

---
+++ src/_modules/clearPhotoFolder.ts
@@ -0,0 +1,30 @@
+import type Order from '../../classes/Order';
+
+/**
+ * | Prop | Value |
+ * |----|----|
+ * | status | 'success' or 'failure' or 'error' |
+ * |----|----|
+ * | message | Description of result |
+ */
+export interface fetchResult {
+  status: 'success' | 'failure' | 'error'
+  message: string,
+  data: any,
+}
+
+/**
+ *
+ * @param intOrExt
+ * @param order
+ */
+export default async function clearPhotosFolder(intOrExt: 'interior'|'exterior', order: Order): Promise<fetchResult> {
+  try {
+    const response = await fetch(`https://www.assetval.club/api/snapshotClearImages/${order._id}/${intOrExt}`, {
+      method: 'POST',
+      redirect: 'follow'
+    });
+
+    return await response.json();
+  } catch (err) { console.error(err); }
+}
