74cf1cc62a5940a2361ecd5cec9278c9aa81a276
abourass <abourassa@assetval.com>
2021-04-22 22:39:22 -0600

    :art: Uppyfactory can now be made reactive

--- src/pages/order/_modules/uppyFactory.ts
+++ src/pages/order/_modules/uppyFactory.ts
@@ -1,11 +1,9 @@
 import Uppy from '@uppy/core';
 import XHRUpload from '@uppy/xhr-upload';
-import { toast } from '@zerodevx/svelte-toast';
-import toastThemes from '../../../_modules/toastThemes';
 import type Order from '../../../../classes/Order';
 import type {photoCategoryIDs} from '../_data/exteriorPhotoCategories';
 
-interface VeritasResponse {
+export interface VeritasResponse {
   message: string,
   status: 'success'|'error',
   href: string|Array<string>
@@ -15,7 +13,7 @@ export default function uppyInstance(maxPhotos: number, order: Order, imageCateg
   const intExt = (maxPhotos > 1) ? 'interior' : 'exterior';
 
   // @ts-ignore
-  const uppy =  new Uppy({
+  return new Uppy({
     autoProceed: true,
     restrictions: {
       maxFileSize: 10000000,
@@ -27,27 +25,4 @@ export default function uppyInstance(maxPhotos: number, order: Order, imageCateg
       fieldName: 'files',
       bundle: true,
   });
-
-  uppy.on('complete', async(result): Promise<void> => {
-    console.log('successful files:', result.successful);
-    console.log('failed files:', result.failed);
-
-    if (result.successful.length > 0){
-      if (result.successful.length === 1){ // Exterior Photo
-        const exteriorPhoto = result.successful[0];
-        const {status, message, href}: VeritasResponse = exteriorPhoto.response.body;
-
-        if (status === 'success'){
-          toast.push(message, toastThemes.success);
-          order.photos.exteriorFiles = [...order.photos.exteriorFiles, {name: href as string, category: imageCategory}]
-        } else if (status === 'error') {
-          toast.push(message, toastThemes.error)
-        } else {
-          toast.push(message)
-        }
-      }
-    }
-  });
-
-  return uppy;
 }
