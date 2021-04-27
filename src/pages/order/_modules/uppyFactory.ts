113aaa9ffd977353ee17e8f6790a75c429411dc4
Abourass <abourassa@assetval.com>
2021-04-27 07:47:03 -0600

    :bug: Fixed a state based error regarding uppy.

--- src/pages/order/_modules/uppyFactory.ts
+++ src/pages/order/_modules/uppyFactory.ts
@@ -8,16 +8,29 @@ export default function uppyInstance(maxPhotos: number, order: Order, imageCateg
   const intExt = (maxPhotos > 1) ? 'interior' : 'exterior';
 
   const uppy: Uppy.Uppy<Uppy.StrictTypes> = Uppy<Uppy.StrictTypes>({
-    autoProceed: false,
+    autoProceed: false, // Automatically upload when the photo is chosen
+    id: imageCategory, // Create separate ID's so Uppy can use local storage easier
+    onBeforeFileAdded(currentFile, files) {
+      if (currentFile.name.length > 5){
+        return { // Reduce file name so the mobile phone users can easily see the edit button
+          ...currentFile,
+          name: currentFile.name.substr(0, 5)
+        }
+      } else {
+        return currentFile;
+      }
+    },
     restrictions: {
-      maxFileSize: 10000000,
+      maxFileSize: 100000000,
       maxNumberOfFiles: maxPhotos,
       allowedFileTypes: ['image/*']
-    }}).use(XHRUpload, {
-      endpoint: `https://www.assetval.club/api/snapshotUpload/${order._id}/${intExt}/${imageCategory}`,
-      formData: true,
-      fieldName: 'files',
-      bundle: true,
+    }});
+
+  uppy.use(XHRUpload, {
+    endpoint: `https://www.assetval.club/api/snapshotUpload/${order._id}/${intExt}/${imageCategory}`,
+    formData: true,
+    fieldName: 'files',
+    bundle: true,
   });
 
   uppy.use(ImageEditor, {
