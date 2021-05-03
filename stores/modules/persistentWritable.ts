e0e2caeefc6ca65d6c8e844418704f2766245af5
Abourass <abourassa@assetval.com>
2021-05-03 07:08:44 -0600

    :bulb: Added explicit types

--- stores/modules/persistentWritable.ts
+++ stores/modules/persistentWritable.ts
@@ -1,6 +1,9 @@
 import { writable, get } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
+/**
+ * Updater type -> Sets the initial type as a generic
+ */
 declare type Updater<T> = (value: T) => T;
 
 /**
@@ -9,26 +12,25 @@ declare type Updater<T> = (value: T) => T;
  * @param initialValue
  */
 export default function persistentWritable<T>(key: string, initialValue: T): Writable<T> {
-  const store = writable(initialValue);
+  const store: Writable<T> = writable(initialValue);
   const {subscribe, set} = store;
-  const json = typeof(localStorage) != 'undefined' ? localStorage.getItem(key) : null;
+  const json: string = typeof(localStorage) != 'undefined' ? localStorage.getItem(key) : null;
 
   if (json) set(<T> JSON.parse(json));
 
-  function updateStorage(key: string, value: T) {
+  function updateStorage(key: string, value: T): void {
     if (typeof(localStorage) == 'undefined') return;
 
     localStorage.setItem(key, JSON.stringify(value))
   }
 
   return {
-    set(value: T) {
+    set(value: T): void {
       updateStorage(key, value)
       set(value)
     },
-    update(updater: Updater<T>) {
-      const value = updater(get(store));
-
+    update(updater: Updater<T>): void {
+      const value: T = updater(get(store));
       updateStorage(key, value);
       set(value)
     },
