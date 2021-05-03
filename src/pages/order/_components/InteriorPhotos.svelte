def0d2a165b16eb5215f27991302e335513595e9
Abourass <abourassa@assetval.com>
2021-05-03 10:35:23 -0600

    :art: Interior photos now show up after upload

--- src/pages/order/_components/InteriorPhotos.svelte
+++ src/pages/order/_components/InteriorPhotos.svelte
@@ -9,40 +9,45 @@
   import confirmChoice from '../../../_modules/confirmationDialoge';
   import clearPhotosFolder from '../../../_modules/clearPhotoFolder';
   import TrashIcon from '../../../../components/icons/TrashIcon.svelte';
+  import Image from '../../../../components/layout/Image.svelte';
 
-  const uppy = uppyInstance(50, $order, 'None');
-  uppy.on('file-editor:start', () => { editorExpanded.update(n => n = true) });
-  uppy.on('file-editor:complete', () => { editorExpanded.update(n => n = false) });
-  uppy.on('complete', async(result): Promise<void> => {
+  // Create a new Uppy instance
+  const uppy = uppyInstance(50, $order);
+  // If they open the photo editor, expand our editor modal
+  uppy.on('file-editor:start', () => { editorExpanded.update(n => n = true); });
+  // When they finish or close editing, close the editor modal
+  uppy.on('file-editor:complete', () => { editorExpanded.update(n => n = false); });
+  // On finish upload look for success
+  uppy.on('complete', async (result): Promise<void> => {
     console.log('successful files:', result.successful);
     console.log('failed files:', result.failed);
 
     if (result.successful.length > 0) {
       const interiorPhotos = result.successful;
-      const {status, message, data} = interiorPhotos[0].response.body;
+      const {status, message, data} = interiorPhotos[interiorPhotos.length - 1].response.body;
 
       toastResults(status, message, () => {
         $order = data;
         $vendor.orders.inProgress = [...$vendor.orders.inProgress.filter((order: Order) => order._id !== order._id), $order];
-      })
+      });
     }
   });
 
   const clearInteriorPhotos = async () => {
     const choice = await confirmChoice('Yes, delete all interior photos!');
 
-    if (choice.isConfirmed){
-      const {status, message, data} = await clearPhotosFolder('interior', $order)
-      toastResults(status, message, () => { $order = data; })
+    if (choice.isConfirmed) {
+      const {status, message, data} = await clearPhotosFolder('interior', $order);
+      toastResults(status, message, () => {
+        $order = data;
+      });
     }
-  }
+  };
 </script>
 
 <div class="flex f-row">
   <header class="mt-3 mb-1 p-0 block">
-    <h1 class="mt-0 mr-auto text-3xl font-semibold mb-2">
-      Interior Photos
-    </h1>
+    <h1 class="mt-0 mr-auto text-3xl font-semibold mb-2"> Interior Photos </h1>
   </header>
   <div class="ml-auto flex items-center">
     <div class="rounded-full bg-dark-transparent h-8 w-8 flex items-center justify-center cursor-pointer" on:click={clearInteriorPhotos}>
@@ -51,14 +56,25 @@
   </div>
 </div>
 
-<div class="h-full flex justify-center">
-  <Dashboard
-    uppy={uppy}
-    props={{
+{#if $order.photos.interiorFiles.length > 0}
+  <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-2">
+    {#each $order.photos.interiorFiles as photo}
+      <Image src={photo.href} />
+    {/each}
+  </div>
+{:else} <!-- No interior photos uploaded yet so show uppy -->
+  <div class="h-full flex justify-center">
+    <Dashboard
+      uppy={uppy}
+      props={{
       inline: true,
       height: 300,
       proudlyDisplayPoweredByUppy: false,
       plugins: ['ImageEditor']
     }}
-  />
-</div>
+    />
+  </div>
+{/if}
+
+
+
