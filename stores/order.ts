0e38a385a202d235df96919d82b8c39a218cd1d5
Abourass <abourassa@assetval.com>
2021-05-03 07:31:23 -0600

    :bulb: Added explicit types for order

--- stores/order.ts
+++ stores/order.ts
@@ -1,6 +1,5 @@
 import persistentWritable from './modules/persistentWritable';
+import type {Writable} from 'svelte/store';
 import Order from '../classes/Order';
 
-export const order = persistentWritable('order', new Order());
-
-
+export const order: Writable<Order> = persistentWritable('order', new Order());
