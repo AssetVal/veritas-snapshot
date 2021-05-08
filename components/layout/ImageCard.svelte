65165a6e115032b6252a23b98fd73b3b73b3cb9a
abourass <abourassa@assetval.com>
2021-05-08 00:01:00 -0600

    :sparkles: Created an image card

---
+++ components/layout/ImageCard.svelte
@@ -0,0 +1,11 @@
+<style>
+  .soft-border { border: 1px solid rgba(0,0,0,.125); }
+  .soft-top { border-top: 1px solid rgba(0,0,0,.18); }
+</style>
+
+<div class="relative flex flex-col min-w-0 break-words bg-clip-border rounded soft-border">
+  <slot name="img" class="rounded-t w-full" />
+  <div class="flex-auto p-1 bg-gray-50 soft-top">
+    <slot name="content" />
+  </div>
+</div>
