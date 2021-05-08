38205cdde4cda04112b906929ce4f25271856115
abourass <abourassa@assetval.com>
2021-05-08 00:01:18 -0600

    :sparkles: Created an input component

---
+++ src/_components/layout/Input.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let value = '';
+  export let label = '';
+</script>
+
+<div class="m-2 relative">
+  <label>{label}</label>
+  <input
+    required
+    type="text"
+    class="pt-5 px-3 pb-1 text-sm border-b-2 focus:outline-none focus:border-b-blue-primary focus:black-plum"
+    value={value}>
+</div>
