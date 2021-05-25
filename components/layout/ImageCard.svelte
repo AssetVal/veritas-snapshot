3f17f8c660e4e45b2f7c3dc72d37dbbd8a5089b8
Abourass <abourassa@assetval.com>
2021-05-25 15:19:49 -0600

    :bug:  Image card now have bottom margin, and fit-content

--- components/layout/ImageCard.svelte
+++ components/layout/ImageCard.svelte
@@ -1,9 +1,10 @@
 <style>
   .soft-border { border: 1px solid rgba(0,0,0,.125); }
   .soft-top { border-top: 1px solid rgba(0,0,0,.18); }
+  .h-content { height: fit-content; }
 </style>
 
-<div class="relative flex flex-col min-w-0 break-words bg-clip-border rounded soft-border h-full">
+<div class="relative flex flex-col min-w-0 break-words bg-clip-border rounded soft-border h-content mb-2">
   {#if $$slots.img}
     <slot name="img" class="rounded-t w-full" />
   {/if}
