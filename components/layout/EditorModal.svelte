b0b6aa53f529f21d63c269dab0df2ef6d3598622
Abourass <abourassa@assetval.com>
2021-05-25 15:38:16 -0600

    :bug: Potential bug fix for iPhone editor modal

--- components/layout/EditorModal.svelte
+++ components/layout/EditorModal.svelte
@@ -5,9 +5,13 @@
 <style>
   /* This just makes sure the modal appears above everything */
   .z-top { z-index: 999999999; }
+  .w-95 { width: 95% }
 </style>
 
 
-<div class={$editorExpanded ? 'fixed h-full w-full flex justify-center content-center z-top -mt-8' : 'hidden'} id="EditorModal">
+<div class={$editorExpanded ? 'fixed h-full w-full flex justify-center content-center z-top -mt-8 mx-auto' : 'hidden'}>
+  <div class={$editorExpanded ? 'w-95 flex justify-center content-center mx-auto' : 'hidden'} id="EditorModal">
 
+  </div>
 </div>
+
