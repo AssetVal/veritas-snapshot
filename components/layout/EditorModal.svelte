88e85bf78df98ff766cea83a7c1e4580a7452929
Abourass <abourassa@assetval.com>
2021-05-25 15:18:57 -0600

    :fire: Removed an unused selector

--- components/layout/EditorModal.svelte
+++ components/layout/EditorModal.svelte
@@ -8,6 +8,6 @@
 </style>
 
 
-<div class={$editorExpanded ? 'block fixed h-full w-full flex justify-center content-center z-top -mt-8' : 'hidden'} id="EditorModal">
+<div class={$editorExpanded ? 'fixed h-full w-full flex justify-center content-center z-top -mt-8' : 'hidden'} id="EditorModal">
 
 </div>
