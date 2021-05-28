30853a8987a426ae2b22c170274120b9ec413a2c
Antonio Bourassa <abourassa@assetval.com>
2021-05-28 11:04:58 -0600

    :sparkles: Moving the save mechanism to index so it can capture interior / exterior

--- src/pages/order/index.svelte
+++ src/pages/order/index.svelte
@@ -3,9 +3,42 @@
   import changeUppyTheme from './_modules/changeUppyTheme';
   import ExteriorPhotos from './_components/ExteriorPhotos.svelte';
   import InteriorPhotos from './_components/InteriorPhotos.svelte';
-
+  import postToVeritas from '../../_modules/postToVeritas';
+  import toastResults from '../../_modules/toastResults';
+  import type Order from '../../../classes/Order';
+  import {vendor} from '../../../stores/vendor';
+  
   setTimeout(changeUppyTheme, 180);
   window.onresize = changeUppyTheme;
+
+  const submitOrder = async(event) => {
+    event.preventDefault();
+    // Convert Node-List to Array
+    const interiorInputs = [...event.currentTarget.querySelectorAll('input[data-set="interior"]')];
+    const interiorValues = interiorInputs.map((input: HTMLInputElement) => [input.id, input.value]);
+    const interiorPhotos = $order.photos.interiorFiles.map(file => ({
+      note: interiorValues.filter(valuePair => valuePair[0] === file.name)[0][1],
+      name: file.name,
+    }));
+
+    const exteriorInputs = [...event.currentTarget.querySelectorAll('input[data-set="exterior"]')];
+    const exteriorValues = exteriorInputs.map((input: HTMLInputElement) => [input.id, input.value]);
+
+    console.log(interiorPhotos, exteriorValues);
+
+    /*
+    const {message, status, data} = await postToVeritas('snapshotInteriorSave', {
+      interiorFiles: JSON.stringify(interiorPhotos),
+      vendorID: $vendor._id,
+      orderID: $order._id
+    });
+
+    toastResults(status, message, () => {
+      $order = data;
+      $vendor.orders.inProgress = [...$vendor.orders.inProgress.filter((order: Order) => order._id !== order._id), $order];
+    });
+    */
+  }
 </script>
 
 <style global>
@@ -21,8 +54,14 @@
   [data-uppy-drag-drop-supported=true] .uppy-Dashboard-AddFiles { border: none !important; }
 </style>
 
-<ExteriorPhotos />
+<form on:submit={submitOrder}>
+  <ExteriorPhotos />
 
-{#if $order.services.isInterior}
-  <InteriorPhotos />
-{/if}
+  {#if $order.services.isInterior}
+    <InteriorPhotos />
+  {/if}
+
+  <div class="m-4 flex justify-center">
+    <button type="submit" class="h-11 py-2 px-4 rounded border-blue-650 bg-blue-primary text-white  w-2/3 sm:w-1/3"> Save Order </button>
+  </div>
+</form>
