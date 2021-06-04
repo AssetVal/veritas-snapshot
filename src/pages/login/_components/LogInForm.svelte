20b9ad24b32751cf4e92f8c5793420f70c1730a3
Antonio Bourassa <abourassa@assetval.com>
2021-06-04 09:56:38 -0600

    :art: Updated our login method to be faster

--- src/pages/login/_components/LogInForm.svelte
+++ src/pages/login/_components/LogInForm.svelte
@@ -8,24 +8,23 @@
   let email, password;
 
   // Functions
-  function login(event){
+  async function login(event){
     event.preventDefault();
 
-    logInToVeritas(email, password).then(response => {
-      if (response.status === 'success'){
-        $vendor = {...response.data};
+    const {status, message, data} = await logInToVeritas(email, password);
+    if (status === 'success'){
+      $vendor = {...data};
 
-        if (window.location.pathname === '/login'){
-          $goto('../');
-        } else {
-          /** We want to $goto our current location.
-           *  Since we're now logged in, we shouldn't be redirected to this login page again. **/
-          $goto(window.location.href);
-        }
+      if (window.location.pathname === '/login'){
+        $goto('../');
       } else {
-        toast.push(response.message)
+        /** We want to $goto our current location.
+         *  Since we're now logged in, we shouldn't be redirected to this login page again. **/
+        $goto(window.location.href);
       }
-    })
+    } else { // Log in failed for some reason, let's inform the user why
+      toast.push(message)
+    }
   }
 </script>
 
@@ -73,7 +72,9 @@
   </div>
 
   <div class="m-4 flex justify-center">
-    <button class="h-11 py-2 px-4 rounded border-blue-650 bg-blue-primary text-white w-full"> Sign In </button>
+    <button class="h-11 py-2 px-4 rounded border-blue-650 bg-blue-primary text-white w-full">
+      Sign In 
+    </button>
   </div>
 
   <div class="text-center pt-4">
